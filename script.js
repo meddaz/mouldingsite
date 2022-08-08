9b8062aaccbcf9ee91a0f86155ad4b56e231c1e1
meddaz <40867923+meddaz@users.noreply.github.com>
2022-08-07 18:01:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,468 @@
+window.onload = function () {
+  var series95Imgs = document.getElementById('series95');
+
+  var series97Imgs = document.getElementById('series97');
+var series98Imgs = document.getElementById('series98');
+
+  series95.addEventListener("change", changeImg95);
+
+  series97.addEventListener("change", changeImg97);
+
+series98.addEventListener("change", changeImg98);
+
+
+  function changeImg95() {
+
+    if (series95Imgs.value == "95038") {
+      display95.innerHTML =
+        '<img src="https://i.imgur.com/SCqKWzy.jpg">';
+
+
+    }
+
+    else if (series95Imgs.value == "95040 ") {
+      display95.innerHTML =
+
+        "<img src='https://i.imgur.com/1BKTHK3.jpg'>";
+
+    }
+
+    else if (series95Imgs.value == "95041") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/HjBJW18.jpg">';
+    }
+
+    else if (series95Imgs.value == "95061") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/0ACX8uv.jpg">';
+    }
+
+    else if (series95Imgs.value == "95211") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/p1f28Xy.jpg">';
+    }
+
+    else if (series95Imgs.value == "95216") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/1Ykau0o.jpg">';
+    }
+    else if (series95Imgs.value == "95219") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/QJcXQxe.jpg">';
+    }
+    else if (series95Imgs.value == "95303") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/1P0BvGw.jpg">';
+    }
+    else if (series95Imgs.value == "95304") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/ZpBrBV3.jpg">';
+    }
+    else if (series95Imgs.value == "95308") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/p8AWcSI.jpg">';
+    }
+    else if (series95Imgs.value == "95338") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/kd3lGY3.jpg">';
+    }
+    else if (series95Imgs.value == "95605") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/WzRVb6M.jpg">';
+    }
+    else if (series95Imgs.value == "95605") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/fzT4oQp.jpg">';
+    }
+    else if (series95Imgs.value == "95608") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/pqFubkO.jpg">';
+    }
+    else if (series95Imgs.value == "95621") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/oUVymp1.jpg">';
+    }
+    else if (series95Imgs.value == "95626") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/3KOVNIy.jpg">';
+    }
+
+    else if (series95Imgs.value == "95653+95653-1") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/hVKDp64.jpg">';
+    }
+    else if (series95Imgs.value == "95663") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/wHIRxsc.jpg">';
+    }
+    else if (series95Imgs.value == "95663+95663-1") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/WqDX4QZ.jpg">';
+    }
+    else if (series95Imgs.value == "95667") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/i7aUqOV.jpg">';
+    }
+    else if (series95Imgs.value == "95688") {
+      display95
+        .innerHTML =
+        '<img src=" https://i.imgur.com/AQWkBfg.jpg">';
+    }
+    else if (series95Imgs.value == "95768") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/fYddbtJ.jpg ">';
+    }
+
+    else if (series95Imgs.value == "95772") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/QZIFGVh.jpg">';
+    }
+
+    else if (series95Imgs.value == "95774") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/lwMoUzb.jpg">';
+    }
+
+    else if (series95Imgs.value == "95787") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/RmDeuO9.jpg ">';
+    }
+
+    else if (series95Imgs.value == "95812") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/HpYHCiU.jpg">';
+    }
+
+    else if (series95Imgs.value == "95814") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/pt4PlvM.jpg">';
+    }
+
+    else if (series95Imgs.value == "95819") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/zWDlGrf.jpg">';
+    }
+
+    else if (series95Imgs.value == "95821") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/Kjh6zSz.jpg">';
+    }
+    else if (series95Imgs.value == "95828") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/BFx62Du.jpg">';
+    }
+
+    else if (series95Imgs.value == "95870") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/2WwHN2k.jpg">';
+    }
+    else if (series95Imgs.value == "95883") {
+      display95
+        .innerHTML =
+        '<img src="https://i.imgur.com/UENigi4.jpg ">';
+    
+    }
+
+
+
+  }
+
+ 
+  function changeImg97() {
+    if (series97Imgs.value == "97001") {
+      display97.innerHTML =
+        '<img src="https://i.imgur.com/N5XHwiK.jpg">';
+
+
+    }
+
+    else if (series97Imgs.value == "97002") {
+      display97.innerHTML = `<img src="https://i.imgur.com/2Rd1nHF.jpg">`;
+
+
+    } else if (series97Imgs.value == "97003") {
+      display97.innerHTML = `<img src="https://i.imgur.com/eVoG2yw.jpg">`;
+
+
+    }
+
+    else if (series97Imgs.value == "97004") {
+      display97.innerHTML = `<img src="https://i.imgur.com/6giiidp.jpg">`;
+
+
+    } else if (series97Imgs.value == "97005") {
+      display97.innerHTML = `<img src="https://i.imgur.com/jAlwaTc.jpg">`;
+
+
+    }
+
+    else if (series97Imgs.value == "97010") {
+      display97.innerHTML = `<img src="https://i.imgur.com/WE1VpYL.jpg">`;
+
+
+    } else if (series97Imgs.value == "97011") {
+      display97.innerHTML = `<img src="https://i.imgur.com/FNAFUm3.jpg">`;
+
+
+    }
+
+    else if (series97Imgs.value == "97012") {
+      display97.innerHTML = `<img src="https://i.imgur.com/ajvni1p.jpg">`;
+
+
+    } else if (series97Imgs.value == "97102") {
+      display97.innerHTML = `<img src="https://i.imgur.com/sTOfeuR.jpg">`;
+
+
+    }
+
+    else if (series97Imgs.value == "97124") {
+      display97.innerHTML = `<img src="https://i.imgur.com/yM40Xwf.jpg">`;
+
+
+    } else if (series97Imgs.value == "https://i.imgur.com/0E3WT3M.jpg") {
+      display97.innerHTML = `<img src="https://i.imgur.com/0E3WT3M.jpg">`;
+
+
+    }
+
+    else if (series97Imgs.value == "97154") {
+      display97.innerHTML = `<img src="https://i.imgur.com/N11xBkd.jpg">`;
+
+
+    } else if (series97Imgs.value == "97164") {
+      display97.innerHTML = `<img src="https://i.imgur.com/DKJSMoU.jpg">`;
+
+
+    }
+
+    else if (series97Imgs.value == "97174") {
+      display97.innerHTML = `<img src="https://i.imgur.com/PUpsRgh.jpg">`;
+
+
+    } else if (series97Imgs.value == "97601") {
+      display97.innerHTML = `<img src="https://i.imgur.com/W4Q4S2k.jpg">`;
+
+
+    }
+
+    else if (series97Imgs.value == "97605") {
+      display97.innerHTML = `<img src="https://i.imgur.com/sVMo2aT.jpg">`;
+
+
+    } else if (series97Imgs.value == "97617") {
+      display97.innerHTML = `<img src="https://i.imgur.com/O0kHB6M.jpg">`;
+
+
+    }
+    else if (series97Imgs.value == "97618") {
+      display97.innerHTML = `<img src="https://i.imgur.com/nj2hQQr.jpg">`;
+
+
+    } else if (series97Imgs.value == "97620") {
+      display97.innerHTML = `<img src="https://i.imgur.com/8sFgnbM.jpg">`;
+    }
+    else if (series97Imgs.value == "97621") {
+      display97.innerHTML = `<img src="https://i.imgur.com/AZz1Esk.jpg">`;
+    }
+    else if (series97Imgs.value == "97622") {
+      display97.innerHTML = `<img src="https://i.imgur.com/gEZYPLX.jpg">`;
+    }
+    else if (series97Imgs.value == "97627") {
+      display97.innerHTML = `<img src="https://i.imgur.com/VZ8Aznn.jpg">`;
+    }
+    else if (series97Imgs.value == "97635") {
+      display97.innerHTML = `<img src="https://i.imgur.com/jGZKkFo.jpg">`;
+    }
+    else if (series97Imgs.value == "97666") {
+      display97.innerHTML = `<img src="https://i.imgur.com/Dj0O4QD.jpg">`;
+    }
+    else if (series97Imgs.value == "97803") {
+      display97.innerHTML = `<img src="https://i.imgur.com/RtEm4hf.jpg">`;
+    }
+    else if (series97Imgs.value == "97901") {
+      display97.innerHTML = `<img src="https://i.imgur.com/Aqnoutq.jpg">`;
+    
+    }
+
+
+
+
+
+  }
+
+  function changeImg98() {
+    if (series98Imgs.value == "") {
+      display98.innerHTML = `<img src="">`;
+
+
+    }
+
+ else if 
+(series98Imgs.value == "98010") {
+  display98.innerHTML = `<img src="https://i.imgur.com/qaLvYxa.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98013") {
+  display98.innerHTML = `<img src="https://i.imgur.com/SGR92zm.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98015") {
+  display98.innerHTML = `<img src="https://i.imgur.com/GF8bJ7w.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98020") {
+  display98.innerHTML = `<img src="https://i.imgur.com/mCfPMOq.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98023") {
+  display98.innerHTML = `<img src="https://i.imgur.com/5C8YO1V.jpg">`;
+}
+ else if
+(series98Imgs.value == "98031") {
+  display98.innerHTML = `<img src="https://i.imgur.com/ZnsTY4v.jpg">`;
+}
+else if 
+(series98Imgs.value == "98063") {
+  display98.innerHTML = `<img src="https://i.imgur.com/bnLGr2P.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98065") {
+  display98.innerHTML = `<img src="https://i.imgur.com/wYQ8sRM.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98068") {
+  display98.innerHTML = `<img src="https://i.imgur.com/brOgXSD.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98069") {
+  display98.innerHTML = `<img src="https://i.imgur.com/YH4YDHh.jpg">`;
+}
+else if 
+(series98Imgs.value == "98080") {
+  display98.innerHTML = `<img src="https://i.imgur.com/U1DYOpp.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98102") {
+  display98.innerHTML = `<img src="https://i.imgur.com/cQdUQbZ.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98202") {
+  display98.innerHTML = `<img src="https://i.imgur.com/AZwPBKT.jpg">`;
+}
+else if 
+(series98Imgs.value == "986602") {
+  display98.innerHTML = `<img src="https://i.imgur.com/rBdaFqY.jpg">`;
+}
+else if 
+(series98Imgs.value == "98605") {
+  display98.innerHTML = `<img src="https://i.imgur.com/0eBgZ7b.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98608") {
+  display98.innerHTML = `<img src="https://i.imgur.com/dj4Yxq9.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98620") {
+  display98.innerHTML = `<img src="https://i.imgur.com/IvqT8Ls.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98621") {
+  display98.innerHTML = `<img src="https://i.imgur.com/pXaMBpb.jpg">`;
+} else if 
+(series98Imgs.value == "98627") {
+  display98.innerHTML = `<img src="https://i.imgur.com/fEGlGw3.jpg">`;
+}
+else if 
+(series98Imgs.value == "98639") {
+  display98.innerHTML = `<img src="https://i.imgur.com/qZCeITB.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98646") {
+  display98.innerHTML = `<img src="https://i.imgur.com/EcwCcpZ.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98652") {
+  display98.innerHTML = `<img src="https://i.imgur.com/iubmbsp.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98658") {
+  display98.innerHTML = `<img src="https://i.imgur.com/XjDKXTB.jpg">`;
+} else if 
+(series98Imgs.value == "98662") {
+  display98.innerHTML = `<img src="https://i.imgur.com/Lg4s1ch.jpg">`;
+}
+else if 
+(series98Imgs.value == "98665") {
+  display98.innerHTML = `<img src="https://i.imgur.com/y8Cuoaw.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98679") {
+  display98.innerHTML = `<img src="https://i.imgur.com/t7sae4Q.jpg">`;
+}
+ else if 
+(series98Imgs.value == "98682") {
+  display98.innerHTML = `<img src="https://i.imgur.com/WxgHQx2.jpg">`;
+}
+else if 
+(series98Imgs.value == "98684") {
+  display98.innerHTML = `<img src="https://i.imgur.com/XWtkdZ2.jpg">`;
+} else if 
+(series98Imgs.value == "98687") {
+  display98.innerHTML = `<img src="https://i.imgur.com/OEx5nA4.jpg">`;
+} else if 
+(series98Imgs.value == "") {
+  display98.innerHTML = `<img src="">`;
+} else if 
+(series98Imgs.value == "98690") {
+  display98.innerHTML = `<img src="https://i.imgur.com/UOgT750.jpg">`;
+}else if 
+(series98Imgs.value == "98701") {
+  display98.innerHTML = `<img src="https://i.imgur.com/P7oBHf5.jpg">`;
+} else if 
+(series98Imgs.value == "98703") {
+  display98.innerHTML = `<img src="https://i.imgur.com/Z1FUiPB.jpg">`;
+}else if 
+(series98Imgs.value == "98704") {
+  display98.innerHTML = `<img src="https://i.imgur.com/z6QqpfT.jpg">`;
+} else if 
+(series98Imgs.value == "98880") {
+  display98.innerHTML = `<img src="https://i.imgur.com/IeFibmg.jpg">`;
+}
+
+else if 
+(series98Imgs.value == "98885") {
+  display98.innerHTML = `<img src="https://i.imgur.com/VWEuQT6.jpg">`;
+} else if 
+(series98Imgs.value == "98888") {
+  display98.innerHTML = `<img src="https://i.imgur.com/xWdlKbJ.jpg">`;
+}
+  }};
